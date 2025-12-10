13ab8f24c3ba4003b103394d1f79ed237c34cd10
immxrtlabeast <imnomakj@mail.ru>
2025-12-11 00:57:48 +0300

    5

---
+++ 5/src/HashFunction.java
@@ -0,0 +1,24 @@
+/**
+ * Абстрактная хэш-функция. Потомок знает, как превратить ключ в число.
+ */
+public abstract class HashFunction<K> {
+    private final int tableSize;
+
+    protected HashFunction(int tableSize) {
+        if (tableSize <= 0) {
+            throw new IllegalArgumentException("Размер таблицы должен быть положительным");
+        }
+        this.tableSize = tableSize;
+    }
+
+    public final int index(K key) {
+        int raw = Math.abs(hash(key));
+        return raw % tableSize;
+    }
+
+    public final int tableSize() {
+        return tableSize;
+    }
+
+    protected abstract int hash(K key);
+}
